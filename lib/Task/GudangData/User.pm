b1688621e6ae689b14b846a184ef30c55da1288d
Steven Haryanto <stevenharyanto@gmail.com>
2013-08-06 01:46:17 +0000

    Import of SHARYANTO/Task-GudangData-User-0.03 from CPAN.
    gitpan-cpan-distribution: Task-GudangData-User
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Task-GudangData-User-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Task/GudangData/User.pm
+++ lib/Task/GudangData/User.pm
@@ -1,12 +1,13 @@
 package Task::GudangData::User;
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 
 1;
 # ABSTRACT: Modules which use data from gudangdata.org
 
 __END__
+
 =pod
 
 =head1 NAME
@@ -15,7 +16,7 @@ Task::GudangData::User - Modules which use data from gudangdata.org
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 DESCRIPTION
 
@@ -30,6 +31,10 @@ as data source.
 
 =head3 L<Business::BankList::Indonesia>
 
+=head3 L<Business::ID::BCA>
+
+=head3 L<Business::ID::Mandiri>
+
 =head3 L<Locale::ID::Locality>
 
 =head3 L<Locale::ID::Province>
@@ -40,10 +45,9 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
